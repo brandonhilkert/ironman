7a77ad9b131527cd5cd651263e642424f87a1055
Brandon Hilkert <brandonhilkert@gmail.com>
2013-01-31 20:04:25 -0500

    Cache.

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,3 +1,8 @@
+//= require "lib/zepto.min"
+//= require "lib/underscore.min"
+//= require "lib/backbone.min"
+//= require "lib/handlebars.min"
+//= require "lib/date.min"
 //= require "app"
 //= require "models"
 //= require "views"
--- lib/project/app.rb
+++ lib/project/app.rb
@@ -7,6 +7,7 @@ module Project
   class App < Sinatra::Base
     set :root, Project.root
     enable  :method_override
+    mime_type :appcache, "text/cache-manifest"
 
     set :sprockets, Sprockets::Environment.new(root) { |env|
       env.append_path(root.join('assets', 'stylesheets'))
@@ -21,6 +22,11 @@ module Project
       def asset_path(source)
         "/assets/" + settings.sprockets.find_asset(source).digest_path
       end
+    end
+
+    get '/offline.appcache' do
+      content_type :appcache
+      erb :"offline.appcache", layout: false
     end
 
     get '/' do
